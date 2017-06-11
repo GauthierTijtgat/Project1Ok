d73f74190d60f522948aacfa5eb620b4ff72456a
GauthierTijtgat <gauthier.tijtgat@student.howest.be>
2017-06-11 15:57:21 +0200

    update dbclass

--- static/addbook.js
+++ static/addbook.js
@@ -1,31 +1,3 @@
 /**
  * Created by Gaultier Tijtgat on 9/06/2017.
  */
-
-$(function () {
-
-    $('#submit').click(function () {
-
-
-        $.ajax({
-            url: '/addbook',
-            data: $('form').serialize(),
-            type: 'post',
-            succes: function (response) {
-                console.log(response);
-
-            },
-            error: function (error) {
-                console.log(error)
-
-            }
-
-
-        });
-
-
-
-    });
-
-
-});

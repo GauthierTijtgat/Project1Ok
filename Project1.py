4e07251fee61fa9f1e8943479b77a66153b89958
GauthierTijtgat <gauthier.tijtgat@student.howest.be>
2017-06-16 17:34:52 +0200

    finalisatie

--- Project1.py
+++ Project1.py
@@ -1,31 +1,60 @@
 from flask import Flask
 from flask import render_template, request
 import os
+import RPi.GPIO as GPIO
+import time
 import _json
 import json
 app = Flask(__name__)
 import mysql.connector
 from dbclass import DbClass
+GPIO.setmode(GPIO.BCM)
+led = 26
+DK = 6
+buz = 19
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(buz, GPIO.OUT)
+GPIO.setup(DK, GPIO.IN)
+GPIO.output(led, GPIO.LOW)
+GPIO.output(buz, GPIO.LOW)
+
 
 listcategorien = ["Educatie", "Technologie", "Actie", "Reizen", "Fantasy"]
+
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+    # def homebutton():
+    #     while True:
+    #
+    #         if DK == GPIO.LOW:
+    #             return render_template('addbook.html')
+    #         else: break
+
+
 
 
 @app.route('/allbooks', methods=["POST","GET"])
 def allbooks():
-    if request.method == 'POST':
-        result = request.form
-        data = DbClass().getdata()
-        #titel = request.form[0][0]('inputTitel')
-        print(result)
-        # print(titel)
-        return render_template("allbooks.html", result=result,data=data)
-    else:
         data = DbClass().getdata()
         return render_template('allbooks.html',data=data)
+
+
+
+
+@app.route('/addedbook',methods=['GET', 'POST'])
+def addedbook():
+    nData = DbClass().adddata()
+    nAuteur = DbClass().addauteur()
+    GPIO.output(led, GPIO.HIGH)
+    GPIO.output(buz, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(led, GPIO.LOW)
+    GPIO.output(buz, GPIO.LOW)
+    return render_template('index.html', nData=nData)
+
 
 
 @app.route('/addbook')
@@ -37,7 +66,15 @@ def rentedbooks():
     return render_template('rentedbooks.html')
 
 
+
+
+
+
+
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT",3000))
 
     app.run(host='0.0.0.0', port=port, debug=True)
+
+
--- dbclass.py
+++ dbclass.py
@@ -1,4 +1,6 @@
+from flask import render_template, request
 class DbClass:
+
     def __init__(self):
         import mysql.connector as connector
 
@@ -13,7 +15,7 @@ class DbClass:
         self.__cursor = self.__connection.cursor()
 
     def getdata(self):
-        q = "select boek.Titel, boek.ISBN, boek.AantalBlz, auteurs.Auteur,categories.Categorie FROM boek JOIN auteurs ON boek.AuteurID=auteurs.AuteurID JOIN categories ON boek.CategorieID=categories.CategorieID;"
+        q = "select boek.Titel, boek.ISBN, boek.AantalBlz, auteurs.Auteur,categories.Categorie FROM  boek JOIN auteurs ON boek.AuteurID=auteurs.AuteurID JOIN categories ON boek.CategorieID=categories.CategorieID;"
         self.__cursor.execute(q)
         data = self.__cursor.fetchall()
         self.__cursor.close()
@@ -21,12 +23,31 @@ class DbClass:
         return data
 
     def adddata(self):
-        q = ""
-        self.__cursor.execute(q)
-        add = self.__cursor.fetchall()
-        self.__cursor.close()
-        print(add)
-        return add
+        print('Entered')
+        nTitel = request.form['inputTitel']
+        print(nTitel)
+        nISBN = request.form['inputISBN']
+        print(nISBN)
+        # nCategorie = request.form['inputCategorie']
+        nAantalblz = request.form['inputAantalblz']
+        print(nAantalblz)
+        self.__cursor.execute("""INSERT INTO mydb.boek(Titel,AantalBlz,ISBN) VALUES (%s,%s,%s)""",(nTitel,nAantalblz,nISBN))
+        self.__connection.commit()
+        print('addedbook')
+
+
+    def addauteur(self):
+        nAuteur = request.form['inputAuteur']
+        print(nAuteur)
+        print("enteredauteur")
+        q = "INSERT INTO mydb.auteurs(Auteur) VALUES ('{auteur}')"
+        qcommand = q.format(auteur = nAuteur)
+        self.__cursor.execute(qcommand)
+        self.__connection.commit()
+        self.__connection.close()
+        print('addedauter')
+
+
 
 
 
